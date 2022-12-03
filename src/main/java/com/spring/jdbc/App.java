b1025a99000152a38d1eb0d69e42fd45ee5da838
Ritesh123-git <67538859+Ritesh123-git@users.noreply.github.com>
2022-12-03 22:58:22 +0530

    springjdbc without xml

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -3,6 +3,7 @@ package com.spring.jdbc;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -19,7 +20,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        ApplicationContext context= new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+//        ApplicationContext context= new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
 //        JdbcTemplate template=context.getBean("jdbcTemplate",JdbcTemplate.class);
         
         
